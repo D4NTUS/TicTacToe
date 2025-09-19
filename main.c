4417d40642020ac513813747843618a25cdacad3
administrator <administrator@Exploreon-Mini>
2025-09-19 11:06:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,20 @@
 int ttt_show_board(){
     char A1='A',A2='A',A3='A',B1='A',B2='A',B3='A',C1='A',C2='A',C3='A';
     system("clear");
-	printf("Tic Tac Toe\n _____ _____ _____\n|     |     |     |\n|  %c  |  %c  |  %c  |\n|     |     |     |\n _____ _____ _____\n|     |     |     |\n|  %c  |  %c  |  %c  |\n|     |     |     |\n _____ _____ _____\n|     |     |     |\n|  %c  |  %c  |  %c  |\n|     |     |     |\n _____ _____ _____\n",A1,A2,A3,B1,B2,B3,C1,C2,C3);
+	printf("Tic Tac Toe\n");
+    printf(" _____ _____ _____\n");
+    printf("|     |     |     |\n");
+    printf("|  %c  |  %c  |  %c  |\n",A1,A2,A3);
+    printf("|     |     |     |\n");
+    printf(" _____ _____ _____\n");
+    printf("|     |     |     |\n");
+    printf("|  %c  |  %c  |  %c  |\n",B1,B2,B3);
+    printf("|     |     |     |\n");
+    printf(" _____ _____ _____\n");
+    printf("|     |     |     |\n");
+    printf("|  %c  |  %c  |  %c  |\n",C1,C2,C3);
+    printf("|     |     |     |\n");
+    printf(" _____ _____ _____\n");
     return 0;
 }
 
