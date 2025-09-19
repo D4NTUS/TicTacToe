09834e6bf20a732dd4673e219528d3ccd2f75c1c
administrator <administrator@Exploreon-Mini>
2025-09-19 10:55:19 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tttshowboard(){
+int ttt_show_board(){
     char A1='A',A2='A',A3='A',B1='A',B2='A',B3='A',C1='A',C2='A',C3='A';
     system("clear");
 	printf("Tic Tac Toe\n _____ _____ _____\n|     |     |     |\n|  %c  |  %c  |  %c  |\n|     |     |     |\n _____ _____ _____\n|     |     |     |\n|  %c  |  %c  |  %c  |\n|     |     |     |\n _____ _____ _____\n|     |     |     |\n|  %c  |  %c  |  %c  |\n|     |     |     |\n _____ _____ _____\n",A1,A2,A3,B1,B2,B3,C1,C2,C3);
